d6888459c76dfcd4af3d0d5e1d93c846777ceec9
Christian Cordero <christiancordero092@gmail.com>
2021-06-30 15:46:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  word.match?(/\A[aeiou]\w/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+|ing\w+/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match?(/^[A-Z]|\W$/)
 end
 
 def valid_phone_number?(phone)
